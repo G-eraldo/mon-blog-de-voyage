2cfbd1a1c90cfc0e2f39d3a6f8ce065d7cbdae70
geraldo <gerald-f@hotmail.fr>
2025-05-02 20:10:45 +0200

    fix: ajouter l'intégration de Vercel Analytics pour le suivi des performances

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import "./globals.css";
-
 // const poppins = Poppins({
 //   variable: "--font-poppins",
 //   subsets: ["latin"],
@@ -31,6 +30,7 @@ export default function RootLayout({ children }) {
         <Header />
         <Navbar />
         <main className="grow">{children}</main>
+        <Analytics />
         <Footer />
       </body>
     </html>
