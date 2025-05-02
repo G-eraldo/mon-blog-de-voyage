eb4f62ceca96fcb8b3e7495df28eb8e622b000a6
geraldo <gerald-f@hotmail.fr>
2025-05-02 16:58:33 +0200

    fix: améliorer la mise en page des champs du formulaire de contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -24,30 +24,30 @@ export default function page() {
       onSubmit={handleSubmit}
       className="flex flex-col items-center gap-6 text-amber-50"
     >
-      <label className="floating-label">
+      <label className="form-control w-full max-w-md floating-label mb-4">
         <span>Ton prénom</span>
         <input
           type="text"
           placeholder="Ton prénom"
-          className="input input-md"
+          className="input input-md w-full"
           name="name"
         />
       </label>
-      <label className="floating-label">
+      <label className="form-control w-full max-w-md floating-label mb-4">
         <span>Ton Email</span>
         <input
           type="mail"
           placeholder="Ton Email"
-          className="input input-md"
+          className="input input-md w-full"
           name="email"
         />
       </label>
-      <label className="floating-label">
+      <label className="form-control w-full max-w-md floating-label mb-4">
         <span>Votre message</span>
         <textarea
           type="textarea"
           placeholder="Votre message"
-          className="input input-md text-wrap h-20"
+          className="input input-md text-wrap h-20 w-full"
           name="message"
         />
       </label>
